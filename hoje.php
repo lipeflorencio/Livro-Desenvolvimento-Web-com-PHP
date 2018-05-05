2bb641e8d677c5a0e47af6d154d1a7d79c7f23eb
Felipe Florencio da Silva <lipeflorencio@gmail.com>
2018-05-04 21:40:53 -0300

    Incluindo frase de saudação

--- hoje.php
+++ hoje.php
@@ -3,6 +3,18 @@
 		<title>Dia <?php echo date('d'); ?></title>
 	</head>
 	<body>
+		<h1>
+			<?php  
+				if ( date('H') >= 0 && date('H') < 12 ) {
+					echo "Bom dia!";
+				} elseif ( date('H') < 18 ) {
+					echo "Boa tarde!";
+				} else {
+					echo "Boa noite!";
+				}
+				
+			?>
+		</h1>
 		<h1>Estamos em <?php echo date('Y'); ?></h1>
 		<p>
 			Agora são <?php echo date('H'); ?> horas e <?php echo date('i'); ?> minutos.
