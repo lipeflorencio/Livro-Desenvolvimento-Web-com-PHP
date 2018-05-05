becca1ab8cbec419b3fa1c0a57aabf1339cf4bf2
Felipe Florencio da Silva <lipeflorencio@gmail.com>
2018-05-04 22:53:51 -0300

    Exibindo o calendário do mês corrente
    Domingo, sábado e dia atual formatados.

--- calendario.php
+++ calendario.php
@@ -1,13 +1,28 @@
 <?php
 	function linha( $semana )
 	{
+		$formatoInicio = "";
+		$formatoFinal = "";
 		echo "<tr>";
 		for ($i = 0; $i <=6; $i++) {
 			if (isset($semana[$i])) {
-				echo "<td>{$semana[$i]}</td>";
+				if ( $i == 0 ) {
+					$formatoInicio = "<font color='red'>";
+					$formatoFinal = "</font>";
+				} elseif ( $i == 6 ) {
+					$formatoInicio = "<b>";
+					$formatoFinal = "</b>";
+				}
+				if ( $semana[$i] == date('d') && $i != 6 ) {
+					$formatoInicio .= "<b>";
+					$formatoFinal = "</b>" . $formatoFinal; 
+				}
+				echo "<td>{$formatoInicio}{$semana[$i]}{$formatoFinal}</td>";
 			} else {
 				echo "<td></td>";
 			}
+			$formatoInicio = "";
+			$formatoFinal = "";
 		}
 		echo "</tr>";
 	}
@@ -16,7 +31,17 @@
 	{
 		$dia = 1;
 		$semana = array();
-		while ($dia <= 31) {
+		
+		$inicio_mes = date('N',mktime(0, 0, 0, date('m') , 1 , date('Y')));
+		$ultimo_dia = date("t");
+		
+		if ( $inicio_mes < 7 ) {
+			for ( $i = 1; $i <= $inicio_mes; $i++ ) {
+				array_push($semana, "");
+			}
+		}
+		
+		while ($dia <= $ultimo_dia) {
 			array_push($semana, $dia);
 			
 			if (count($semana) == 7) {
@@ -30,6 +55,13 @@
 	}
 	
 ?>
+
+<h3> Calendário do mês de 
+	<?php 
+		setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+		echo strftime('%B', strtotime('today'));
+	?> 
+</h3>
 
 <table border="1">
 	<tr>
